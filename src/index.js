e00854c4a136cfdeec5c6ac05008bc498ee97c71
MaxShylov <shma9013@gmai.com>
2018-12-15 01:59:58 +0200

    save

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <Router>
-    <Route path='/' component={App} />
+    <Route path='*' component={App} />
   </Router>,
   document.getElementById('root'));
 
