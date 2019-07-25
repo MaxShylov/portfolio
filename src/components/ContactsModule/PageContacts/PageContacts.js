8d57039efb7282da7fcbb7a10ea7369e7ba53f54
max.is.genius.fullstackdev <max.is.genius.fullstackdev@gmail.com>
2019-07-25 18:21:41 +0300

    update tel

--- src/components/ContactsModule/PageContacts/PageContacts.js
+++ src/components/ContactsModule/PageContacts/PageContacts.js
@@ -4,7 +4,7 @@ import List from 'antd/lib/list';
 import { ContactItem } from '../';
 
 const data = [
-  { icon: 'phone', content: '+38 (093) 055-71-31', type: 'phone' },
+  { icon: 'phone', content: '+38 (093) 777-22-77', type: 'phone' },
   { icon: 'mail', content: 'max.shylov.17@gmail.com', type: 'email' },
   { icon: 'github', content: 'https://github.com/MaxShylov', type: 'link' },
   { icon: 'facebook', content: 'https://www.facebook.com/max.shylov', type: 'link' },
